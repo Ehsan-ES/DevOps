58f4461dc4d27c471b47e584d06b22a9b91452eb
ehsan so <ehsansohrabi25790@gmail.com>
2025-05-06 00:02:32 +0330

    modified disk check files

--- system-monitor/disk_check.sh
+++ system-monitor/disk_check.sh
@@ -3,6 +3,7 @@
 THRESHOLD=80
 
 USAGE=$(df -h / | grep -v Filesystem | awk '{print $5}' | cut -d'%' -f1)
+#USGE=$(df -h / | grep -v Flesystem | awk '{print $5}' | cut -d'%' -f1)
 
 if [ $USAGE -gt $THRESHOLD ];then
 	echo "WARNING: DISK CAPACITY IS $USAGE% !!"
