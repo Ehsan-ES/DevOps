382893a79b523319c2e959974bd1110f29784bd4
mahan <mahansohrabi25790@gmail.com>
2025-05-06 12:27:59 +0330

    created test.txt and modify disk_check

--- system-monitor/disk_check.sh
+++ system-monitor/disk_check.sh
@@ -2,13 +2,13 @@
 
 THRESHOLD=80
 
-USAGE=$(df -h / | grep -v Filesystem | awk '{print $5}' | cut -d'%' -f1)
-#USGE=$(df -h / | grep -v Flesystem | awk '{print $5}' | cut -d'%' -f1)
+#USAGE=$(df -h / | grep -v Filesystem | awk '{print $5}' | cut -d'%' -f1)
+USAGE=$(df -h / | grep -v Flesystem | awk '{print $5}')
 
 if [ $USAGE -gt $THRESHOLD ];then
-	echo "WARNING: DISK CAPACITY IS $USAGE% !!"
+	echo "WARNING: DISK CAPACITY IS $USAGE !!"
 else
-	echo "DISK CAPACITY HAS NOT PROBLEM : $USAGE%"
+	echo "DISK CAPACITY HAS NOT PROBLEM : $USAGE ..!! - I am Ehsan"
 fi
 
 
